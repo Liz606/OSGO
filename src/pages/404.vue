c28c4f6369b413dc1afe2d433771e2777d84bc1f
Lizzy <1105394023@qq.com>
2017-05-10 21:11:15 +0800

    inner snap.svg

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,24 +1,38 @@
 <template>
   <main-layout>
-  <div class="content-container">
-  	<p>Page not found</p>
-  </div>
+  
+    <div class="content-container">
+  
+      <p>Page not found</p>
+  
+    </div>
+  
   </main-layout>
 </template>
 
 <script>
   import MainLayout from '../layouts/Main.vue'
+  
   export default {
+  
     components: {
+  
       MainLayout
+  
     }
+  
   }
 </script>
 <style scoped>
-.content-container{
-	width: 100%;
-	max-width: 1024px;
-	margin: 0 auto;
-	min-height: 300px;
-}
+  .content-container {
+  
+    width: 100%;
+  
+    max-width: 1024px;
+  
+    margin: 0 auto;
+  
+    min-height: 300px;
+  
+  }
 </style>
