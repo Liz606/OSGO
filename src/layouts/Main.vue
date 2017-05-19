493a997d2da51885a486b5acead719df4392fbdf
Lizzy <1105394023@qq.com>
2017-05-19 11:11:52 +0800

    debug

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -21,11 +21,11 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#"><img src="../assets/images/blue-logo.png" height="50px"></a>
+          <a class="navbar-brand" href="/"><img src="../assets/images/blue-logo.png" height="50px"></a>
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">首页<span class="sr-only">(current)</span></a></li>
+            <li class="active"><a href="/">首页<span class="sr-only">(current)</span></a></li>
             <li><a href="#">关于</a></li>
           </ul>
           <form class="navbar-form navbar-right">
--- src/pages/CourseState.vue
+++ src/pages/CourseState.vue
@@ -32,137 +32,133 @@
   	import MainLayout from '../layouts/View.vue'
     import VLink from '../components/VLink.vue'
   	import '../assets/snap/snap.svg.0.5.1.js'
+    import $ from '../assets/jquery/jquery.js'
   	export default {
   	  components: {
   	    MainLayout,
   	    VLink
-  	  }
+  	  },
+      mounted:function (){
+        function CourseState(){
+
+          var s = Snap("#svg");
+          s.clear();
+          //进程调度
+          var path1 = s.paper.line(450, 220, 500, 320).attr({
+          stroke: "#fff",
+          strokeWidth: 5  
+          });
+          //时间片完
+          var path2 = s.paper.line(470, 220, 520, 320).attr({
+          stroke: "#fff",
+          strokeWidth: 5  
+          });
+          //I/O完成
+          var path3 = s.paper.line(270, 220, 220, 320).attr({
+          stroke: "#fff",
+          strokeWidth: 5  
+          });
+          //I/O请求
+          var path4 = s.paper.line(250, 380, 480, 380).attr({
+          stroke: "#fff",
+          strokeWidth: 5  
+          });
+          var bigCircle = s.paper.circle(375, 80, 50).attr({
+          fill: "#44cef6",
+          stroke: "#ffffff",
+          strokeWidth: 5
+          });
+          
+          var stateRect1 =s.paper.group(s.paper.rect(300, 150,150,50,5,5).attr({
+          fill: "#44cef6"
+          }),s.paper.text(370, 180,'就绪状态').attr({
+          fill: "#fff",
+          'font-weight': 100,
+          'font-size': 20,
+          'text-anchor': 'middle',
+          })) 
+          
+          var stateRect2 =s.paper.group(s.paper.rect(500, 350,150,50,5,5).attr({
+          fill: "#9ed048"
+          }),s.paper.text(570, 380,'运行状态').attr({
+          fill: "#fff",
+          'font-weight': 100,
+          'font-size': 20,
+          'text-anchor': 'middle',
+          })) 
+          
+          var stateRect3 =s.paper.group(s.paper.rect(75, 350,150,50,5,5).attr({
+          fill: "#ffa400"
+          }),s.paper.text(150, 380,'阻塞状态').attr({
+          fill: "#fff",
+          'font-weight': 100,
+          'font-size': 20,
+          'text-anchor': 'middle',
+          }))
+          
+          stateRect1.click(function(){
+              runningToReady()
+          });
+          stateRect2.click(function(){
+              readyToRunning()
+          });
+          stateRect3.click(function(){
+              runningToBlocked()
+          });
+          
+          function readyToRunning(){
+            bigCircle.attr({
+            cx: 375,
+            cy: 80,
+            fill:'#44cef6'
+            })
+            bigCircle.animate({
+            cx: 580,
+            cy: 280,
+            fill:'#9ed048'
+            }, 100);
+          }
+          function runningToReady(){
+            bigCircle.attr({
+            cx: 580,
+            cy: 280,
+            fill:'#9ed048'
+            })
+            bigCircle.animate({
+            cx: 375,
+            cy: 80,
+            fill:'#44cef6'
+            }, 100);
+          }
+          function runningToBlocked(){
+            bigCircle.attr({
+            cx: 580,
+            cy: 280,
+            fill:'#9ed048'
+            })
+            bigCircle.animate({
+            cx: 150,
+            cy: 280,
+            fill:'#ffa400'
+            }, 100);
+          }
+          function blockedToReady(){
+            bigCircle.attr({
+            cx: 150,
+            cy: 280,
+            fill:'#ffa400'
+            })
+            bigCircle.animate({
+            cx: 580,
+            cy: 280,
+            fill:'#9ed048'
+            }, 100);
+          }
+        }
+        CourseState();
+      }
   	}
 
-    var s = Snap("#svg");
-    // function courseStateSVG(){
-    //       s.clear();
-    //       //进程调度
-    //       var path1 = s.paper.line(450, 220, 500, 320).attr({
-    //       stroke: "#fff",
-    //       strokeWidth: 5  
-    //       });
-    //       //时间片完
-    //       var path2 = s.paper.line(470, 220, 520, 320).attr({
-    //       stroke: "#fff",
-    //       strokeWidth: 5  
-    //       });
-    //       //I/O完成
-    //       var path3 = s.paper.line(270, 220, 220, 320).attr({
-    //       stroke: "#fff",
-    //       strokeWidth: 5  
-    //       });
-    //       //I/O请求
-    //       var path4 = s.paper.line(250, 380, 480, 380).attr({
-    //       stroke: "#fff",
-    //       strokeWidth: 5  
-    //       });
-    //       var bigCircle = s.paper.circle(375, 80, 50).attr({
-    //       fill: "#44cef6",
-    //       stroke: "#ffffff",
-    //       strokeWidth: 5
-    //       });
-          
-          
-    //       var stateRect1 =s.paper.group(s.paper.rect(300, 150,150,50,5,5).attr({
-    //       fill: "#44cef6"
-    //       }),s.paper.text(370, 180,'就绪状态').attr({
-    //       fill: "#fff",
-    //       'font-weight': 100,
-    //       'font-size': 20,
-    //       'text-anchor': 'middle',
-    //       })) 
-          
-          
-    //       var stateRect2 =s.paper.group(s.paper.rect(500, 350,150,50,5,5).attr({
-    //       fill: "#9ed048"
-    //       }),s.paper.text(570, 380,'运行状态').attr({
-    //       fill: "#fff",
-    //       'font-weight': 100,
-    //       'font-size': 20,
-    //       'text-anchor': 'middle',
-    //       })) 
-          
-          
-    //       var stateRect3 =s.paper.group(s.paper.rect(75, 350,150,50,5,5).attr({
-    //       fill: "#ffa400"
-    //       }),s.paper.text(150, 380,'阻塞状态').attr({
-    //       fill: "#fff",
-    //       'font-weight': 100,
-    //       'font-size': 20,
-    //       'text-anchor': 'middle',
-    //       }))
-          
-          
-          
-          
-          
-          
-    //       stateRect1.click(function(){
-    //       runningToReady()
-    //       });
-    //       stateRect2.click(function(){
-    //       readyToRunning()
-    //       });
-    //       stateRect3.click(function(){
-    //       runningToBlocked()
-    //       });
-          
-    //       function readyToRunning(){
-    //         bigCircle.attr({
-    //         cx: 375,
-    //         cy: 80,
-    //         fill:'#44cef6'
-    //         })
-    //         bigCircle.animate({
-    //         cx: 580,
-    //         cy: 280,
-    //         fill:'#9ed048'
-    //         }, 100);
-    //       }
-    //       function runningToReady(){
-    //         bigCircle.attr({
-    //         cx: 580,
-    //         cy: 280,
-    //         fill:'#9ed048'
-    //         })
-    //         bigCircle.animate({
-    //         cx: 375,
-    //         cy: 80,
-    //         fill:'#44cef6'
-    //         }, 100);
-    //       }
-    //       function runningToBlocked(){
-    //         bigCircle.attr({
-    //         cx: 580,
-    //         cy: 280,
-    //         fill:'#9ed048'
-    //         })
-    //         bigCircle.animate({
-    //         cx: 150,
-    //         cy: 280,
-    //         fill:'#ffa400'
-    //         }, 100);
-    //       }
-    //       function blockedToReady(){
-    //         bigCircle.attr({
-    //         cx: 150,
-    //         cy: 280,
-    //         fill:'#ffa400'
-    //         })
-    //         bigCircle.animate({
-    //         cx: 580,
-    //         cy: 280,
-    //         fill:'#9ed048'
-    //         }, 100);
-    //       }
-    //       }
-    // courseStateSVG();
 
 </script>
 <style scoped>
